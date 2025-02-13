af29e48a9e2a932ba996fdfab9ff62aa560cb95a
Victor Aires <victorairesferreirac@gmail.com>
2025-02-13 13:51:53 -0300

    started hospital.c

---
+++ hospital.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <time.h>
+
+typedef struct
+{
+    int identificador;
+    char nome[40];
+    char especialidade[40];
+} medico;
+
+typedef struct
+{
+    int indentificador;
+    char nome[40];
+    int identidade;
+    char endereco[40];
+    int telefone;
+    char sexo;
+} paciente;
+
+typedef struct
+{
+    int numero;
+    int medicoID;
+    int pacienteID;
+    int horario;
+    int duracao;
+    int data;
+} consulta;
+
+int main(void)
+{
+    while (1)
+    {
+        int *incluir;
+        incluir = malloc(100(int));
+
+        free Incluir;
+    }
+    return 0;
+}
